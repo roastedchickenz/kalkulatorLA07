45f77882504977f7e1ac73456e699767a698381b
roastedchickenz <timotiuswellypranata@gmail.com>
2021-12-22 01:21:06 -0600

    menambah fungsi perkalian

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ def pengurangan (a, b):
 	total = a-b
 	return total
 
+def perkalian (a,b):
+	total = a*b
+	return total
 def main():
 	print(penambahan(3, 4))
-
+	print(pengurangan(10,5))
+	print(perkalian(10, 10))
 main()
